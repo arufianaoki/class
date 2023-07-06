320754a6bed0ecf9a8aec1df81acee05e4b6d55d
arufianaoki <110089332+arufianaoki@users.noreply.github.com>
2023-07-06 11:11:56 +0700

    Add files via upload

---
+++ moviedb/http_helper.dart
@@ -0,0 +1,59 @@
+//kode http_helper.dart
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:moviedb/model/Movie.dart';
+
+class HttpHelper {
+/*
+Kelas ini digunakan untuk mendapatkan data dari themoviedb
+dengan metode Upcoming yang memberikan nilai return berupa teks
+*/
+
+  final String urlKey = 'api_key=b9da0a162ee87bfb78f86c45c7162cd6';
+  final String urlBase = 'https://api.themoviedb.org/3/movie';
+  final String urlUpcoming = '/upcoming?';
+  final String urlLanguage = '&language=en-US';
+  final String urlSearchBase = 'https://api.themoviedb.org/3/search/movie?'
+      'api_key=b9da0a162ee87bfb78f86c45c7162cd6&query=';
+
+  Future<String> getUpcoming() async {
+    final Uri upcoming =
+        Uri.parse(urlBase + urlUpcoming + urlKey + urlLanguage);
+
+    http.Response result = await http.get(upcoming);
+    if (result.statusCode == HttpStatus.ok) {
+      String responseBody = result.body;
+      return responseBody;
+    } else {
+      return '{}';
+    }
+  }
+
+  Future<List> getUpComingAsList() async {
+    final Uri upcoming =
+        Uri.parse(urlBase + urlUpcoming + urlKey + urlLanguage);
+    http.Response result = await http.get(upcoming);
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponseBody = json.decode(result.body);
+      final movieObjects = jsonResponseBody['results'];
+      List movies = movieObjects.map((json) => Movie.fromJson(json)).toList();
+      return movies;
+    } else {
+      return [];
+    }
+  }
+
+  Future<List> findMovies(String title) async {
+    final Uri query = Uri.parse(urlSearchBase + title);
+    http.Response hasilCari = await http.get(query);
+    if (hasilCari.statusCode == HttpStatus.ok) {
+      final jsonResponseBody = json.decode(hasilCari.body);
+      final movieObjects = jsonResponseBody['results'];
+      List movies = movieObjects.map((json) => Movie.fromJson(json)).toList();
+      return movies;
+    } else {
+      return [];
+    }
+  }
+}
---
+++ moviedb/movie_detail.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:moviedb/model/Movie.dart';
+
+class MovieDetail extends StatelessWidget {
+  final Movie selectedMovie;
+  const MovieDetail({Key? key, required this.selectedMovie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String path;
+    double screenHeight = MediaQuery.of(context).size.height; //Tinggi Layar
+
+    if (selectedMovie.posterPath != null) {
+      path = 'https://image.tmdb.org/t/p/w500/${selectedMovie.posterPath}';
+    } else {
+      path =
+          'https://images.freeimages.com/images/large-oreviews/5eb/movie-clapboard-1184339.jpg';
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('${selectedMovie.title}'),
+        ),
+        body: SingleChildScrollView(
+          //Fitur Scroll
+          child: Center(
+              //Widget Tengah
+              child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(16), //Padding
+                height: screenHeight / 1.5, //Tinggi Gambar
+                child: Image.network(path),
+              ),
+              Text('${selectedMovie.overview}'),
+            ],
+          )),
+        ));
+  }
+}
